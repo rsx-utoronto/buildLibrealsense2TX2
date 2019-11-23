91a50516fa53d24bc96a188a9b07738dab4b99a8
Hudson Schier <hudsonschier@gmail.com>
2019-11-23 02:46:12 -0500

    Changed to latest versions

--- scripts/getKernelSourcesNoGUI.sh
+++ scripts/getKernelSourcesNoGUI.sh
@@ -6,7 +6,7 @@ cd /usr/src
 # wget -N https://developer.nvidia.com/embedded/dlc/l4t-sources-32-1-JAX-TX2
 # wget -N https://developer.nvidia.com/embedded/dlc/l4t-sources-31-1-0
 # l4t-sources-31-1-0 is a tbz2 file
-wget -N https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/sources/public_sources.tbz2
+wget -N https://developer.nvidia.com/embedded/r32-2-3_Release_v1.0/Sources/T186/public_sources.tbz2
 sudo tar -xvf public_sources.tbz2 public_sources/kernel_src.tbz2
 tar -xvf public_sources/kernel_src.tbz2
 #tar -xvf l4t-sources-32-1-JAX-TX2 public_sources/kernel_src.tbz2
